2b438ee8ea3de2568af98b609168cc54d3a10aa7
Stijn Ruts <git@stijnruts.be>
2026-04-19 18:17:35 +0200

    follows nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,8 +2,14 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
-    devshell.url = "github:numtide/devshell";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
   };
 
