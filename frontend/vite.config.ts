6dc05bcab913aa40a9acaa2ed1d42eec878cf92a
Stijn Ruts <git@stijnruts.be>
2026-04-26 10:05:57 +0200

    WIP vituum, nunjucks, favicons

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,62 +1,43 @@
 import type { UserConfig } from "vite";
-import unpluginFavicons from "@anolilab/unplugin-favicons/vite";
+import { ViteFaviconsPlugin } from 'vite-plugin-favicon';
+import vituum from 'vituum';
 import nunjucks from "@vituum/vite-plugin-nunjucks";
 
-import fs from "fs/promises";
-import path from "path";
-
 export default {
-  root: "src",
   build: {
-    outDir: "../dist",
-    emptyOutDir: true,
     rollupOptions: {
-      input: ["index.njk.html", "error.njk"],
+      input: ['src/pages/**/*.njk']
     },
   },
-  plugins: [
-    unpluginFavicons({
-      logo: "./src/favicon.svg",
-      outputPath: "assets/favicons",
-      favicons: {
-        path: "/assets/favicons",
-        appName: "My Project",
-        appShortName: "My Project",
-        appDescription: "My new project",
-        lang: "en",
-        developerName: null,
-        developerURL: null,
-        background: "#ddd",
-        theme_color: "#333",
-        icons: {
-          favicons: true,
-          android: true,
-          appleIcon: true,
-          appleStartup: false,
-          windows: false,
-          yandex: false,
-        },
-      },
-    }),
-    nunjucks(),
-    {
-      name: "rename-njk-html",
-      closeBundle: async () => {
-        const dist = path.resolve("dist");
-        const files = await fs.readdir(dist);
-
-        for (const file of files) {
-          if (file.endsWith(".njk.html")) {
-            const newName = file.replace(".njk.html", ".html");
-            await fs.rename(path.join(dist, file), path.join(dist, newName));
-          }
-        }
-      },
-    },
-  ],
   server: {
     allowedHosts: ["myproject-dev.local"],
     port: 8001,
     strictPort: true,
   },
+  plugins: [
+    vituum(),
+    nunjucks({
+      root: './src',
+    }),
+    ViteFaviconsPlugin({
+      logo: "./src/assets/favicon.svg",
+      favicons: {
+        appName: "My Project",
+        appShortName: "My Project",
+        appDescription: "My new website project",
+        lang: "en",
+        developerName: null,
+        developerURL: null,
+        background: "#3B88C3",
+        theme_color: "#FFF",
+        icons: {
+          favicons: true,
+          android: false,
+          appleIcon: false,
+          appleStartup: false,
+          windows: false,
+        },
+      },
+    }),
+  ],
 } satisfies UserConfig;
